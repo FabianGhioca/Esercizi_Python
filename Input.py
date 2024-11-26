a91b7b051a5edf092e2633ca487a28225a22b5e3
FabianGhioca <ghiocafabian2004@gmail.com>
2024-11-26 11:55:50 +0100

    Input

---
+++ Input.py
@@ -0,0 +1,9 @@
+# Esercizio repository GitHub INPUT 
+
+numero1 = int(input("inserisci il primo numero: "))
+numero2 = int(input("inserisci il secondo numero: "))
+numero3 = int(input("inserisci il terzo numero: "))
+print("hai inserito questi valori: ")
+print(+ numero1)
+print(+ numero2)
+print(+ numero3)
