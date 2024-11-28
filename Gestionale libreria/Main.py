aa81c43dbb779dd875ae71b9f462e4f0efd1bced
FabianGhioca <ghiocafabian2004@gmail.com>
2024-11-28 10:32:09 +0100

    esercizio della libreria finito

--- Gestionale libreria/Main.py
+++ Gestionale libreria/Main.py
@@ -1,7 +1,6 @@
 lista_libri = ["libro1", "libro3", "libro5"]
 libri_prestito = ["libro2", "libro4"]
-
-
+from Libro import AggiuntaLibro, PrestitoLibro , RiportaLibro, DisponibilitaLibro, LibriLibreria, LibriPrestito
 
 
 funzione = 0
@@ -16,43 +15,32 @@ while funzione <= 6:
     print("7 - Esci dal programma")
     funzione = int(input("Inserisci il numero della funzione desiderata: "))
 
-    if funzione == 1: # Aggiunta libro
+    if funzione == 1:                                          # Aggiunta libro
         libro_aggiunta = input("Inserisci il titolo del nuovo libro: ")
-        lista_libri.insert(0, libro_aggiunta)
-        print("Hai aggiunto questo libro: " +libro_aggiunta)
-        print("Ecco la lista aggiornata:")
+        AggiuntaLibro(libro_aggiunta, lista_libri)
+
+    elif funzione == 2:                                        # Prestito libro
         for item in lista_libri:
             print(item)
-
-    elif funzione == 2: # Prestito libro
-        for item in lista_libri:
-            print(item)
-        prestito = input("inserisci il libro da prendere in prestito: ")
-        lista_libri.remove(prestito)
-        libri_prestito.insert(0, prestito)
-        print("Hai preso in prestito questo libro: " +prestito)
-
-    elif funzione == 3: # Riporta libro
-        print("Quali di questi libri devi riportare?")
-        print(libri_prestito)
-        libro_riportato = input("Inserisci il libro riportato: ")
-        libri_prestito.remove(libro_riportato)
-        lista_libri.insert(0, libro_riportato)
-        print("Hai riporato questo libro: " +libro_riportato)
-
-    elif funzione == 4: # Disponibilita libro
-        for item in lista_libri:
-            print(item)
-
-    elif funzione == 5: # Libri disponibili nella libreria
-        lista_libreria = lista_libri + libri_prestito
-        for item in lista_libreria:
-            print(item)
-
-    elif funzione == 6: # Libri dati in prestito
+        prestito = input("Seleziona il libro che vuoi prendere in prestito: ")
+        PrestitoLibro(prestito, lista_libri, libri_prestito)
+    
+    elif funzione == 3:                                        # Riporta libro
         for item in libri_prestito:
             print(item)
+        libro_riportato = input("Inserisci il libro riportato: ")
+        RiportaLibro(libro_riportato, lista_libri, libri_prestito)
+        
+    elif funzione == 4:                                        # Disponibilita libro
+        titolo = input("Inserisci il titolo che vuoi cercare: ")
+        DisponibilitaLibro(titolo, lista_libri)
 
-    else: # Fine programma
+    elif funzione == 5:                                        # Libri disponibili nella libreria
+        LibriLibreria(lista_libri)
+
+    elif funzione == 6:                                        # Libri dati in prestito
+        LibriPrestito(libri_prestito)
+
+    else:                                                      # Fine programma
         print("Hai chiuso il programma")
         break
